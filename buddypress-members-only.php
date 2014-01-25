66fe57284f40d14b640d343bef5016295312011b
zhuyi <zhuyi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-25 05:26:41 +0000

    buddypress members only 1.0.0 published
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-members-only/trunk@845023 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buddypress-members-only.php
@@ -0,0 +1,163 @@
+<?php
+/*
+Plugin Name: BuddyPress Members only
+Description: Only registered users can view your site, non members can only see a login/home page with no registration options
+Version: 1.0.0
+Author: Tomas Zhu
+Author URI: http://tomas.zhu.bz/
+Plugin URI: http://tomas.zhu.bz/
+
+Copyright 2014  tomas.zhu  (email : expert2wordpress@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+add_action('admin_menu', 'bp_members_only_option_menu');
+
+function bp_members_only_option_menu()
+{
+
+   add_menu_page(__('Buddypress Members Only', 'BPMO'), __('Buddypress Members Only', 'BPMO'), 0, 'bpmemberonly', 'buddypress_members_only_setting');
+   add_submenu_page('bpmemberonly', __('Buddypress Members Only','BPMO'), __('Buddypress Members Only','BPMO'), 0, 'bpmemberonly', 'buddypress_members_only_setting');
+}
+
+function buddypress_members_only_setting()
+{
+		global $wpdb;
+		$m_bpmoregisterpageurl = get_option('bpmoregisterpageurl');
+
+		if (isset($_POST['bpmosubmitnew']))
+		{
+			if (isset($_POST['bpmoregisterpageurl']))
+			{
+				$m_bpmoregisterpageurl = $wpdb->escape($_POST['bpmoregisterpageurl']);
+			}
+				
+				update_option('bpmoregisterpageurl',$m_bpmoregisterpageurl);
+			
+			tooltipsMessage("Changes saved.");
+		}
+		echo "<br />";
+
+		$saved_register_page_url = get_option('bpmoregisterpageurl');
+		?>
+
+<div style='margin:10px 5px;'>
+<div style='float:left;margin-right:10px;'>
+<img src='<?php echo get_option('siteurl');  ?>/wp-content/plugins/buddypress-members-only/images/new.png' style='width:30px;height:30px;'>
+</div> 
+<div style='padding-top:5px; font-size:22px;'> <i></>Buddypress Members Only Setting:</i></div>
+</div>
+<div style='clear:both'></div>		
+		<div class="wrap">
+			<div id="dashboard-widgets-wrap">
+			    <div id="dashboard-widgets" class="metabox-holder">
+					<div id="post-body">
+						<div id="dashboard-widgets-main-content">
+							<div class="postbox-container" style="width:90%;">
+								<div class="postbox">
+									<h3 class='hndle'><span>
+										Option Panel:
+									</span>
+									</h3>
+								
+									<div class="inside" style='padding-left:5px;'>
+										<br />
+										<form id="bpmoform" name="bpmoform" action="" method="POST">
+										<table id="bpmotable" width="100%">
+										<tr>
+										<td width="30%">
+										Register Page URL:
+										</td>
+										<td width="70%">
+										<input type="text" id="bpmoregisterpageurl" name="bpmoregisterpageurl" size="70" value="<?php  echo $saved_register_page_url; ?>">
+										</td>
+										</tr>
+										</table>
+										<br />
+										<input type="submit" id="bpmosubmitnew" name="bpmosubmitnew" value=" Submit ">
+										</form>
+										
+										<br />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+		    	</div>
+			</div>
+		</div>
+		<div style="clear:both"></div>
+		<br />
+		
+		<?php
+		}				
+
+	
+function buddypress_members_only_message($p_message)
+{
+
+	echo "<div id='message' class='updated fade'>";
+
+	echo $p_message;
+
+	echo "</div>";
+
+}
+
+function buddypress_only_for_members()
+{
+	if (is_front_page()) return;
+	if ( bp_is_register_page() || bp_is_activation_page() )
+	{
+		return;
+	}
+	$current_url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	$current_url = str_ireplace('http://','',$current_url);
+	$current_url = str_ireplace('www','',$current_url);
+	$saved_register_page_url = get_option('bpmoregisterpageurl');
+	$saved_register_page_url = str_ireplace('http://','',$saved_register_page_url);
+	$saved_register_page_url = str_ireplace('www','',$saved_register_page_url);
+	
+	if (stripos($current_url,$saved_register_page_url) === false)
+	{
+
+	}
+	else 
+	{
+		return;
+	}
+	
+	
+	if ( is_user_logged_in() == false )
+	{
+		if (empty($saved_register_page_url))
+		{
+			$current_url = $_SERVER['REQUEST_URI'];
+			$redirect_url = get_option('siteurl')."/wp-login.php?redirect_to=$current_url";
+			wp_safe_redirect($redirect_url);
+			exit;
+		}
+		else 
+		{
+			$saved_register_page_url = 'http://'.$saved_register_page_url;
+			wp_safe_redirect($saved_register_page_url);
+			exit;
+		}
+	}
+}
+
+add_action('wp','buddypress_only_for_members');
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== BuddyPress Members Only ===
+Contributors: zhuyi
+Author URI: http://tomas.zhu.bz/
+Donate link: http://tomas.zhu.bz/
+Tags:buddypress, community, restricts, membership, user, access, privacy, private, private community, protection, member
+Requires at least: 2.0
+Tested up to: 3.8.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Restricts Your Buddypress to logged in/registered members.
+
+== Description ==
+Plugin Name: BuddyPress Members Only<br>
+Plugin Support URI: http://tomas.zhu.bz/my-bbpress-plugin-bbpress-notification.html/<br>
+
+Restricts Your Buddypress to logged in/registered members only.
+Only registered/logged in users can view your site, non members can only open home page/login/register/lost password pages.
+Logged in users have full access.
+
+If a users who not logged in, he can view home page, but when he try to view any other buddypress content, he will be redirected to optionally URL which can be setting in admin panel.
+
+<h4>How To Use:</h4>
+BuddyPress Members Only is a simple & quick & light BuddyPress Members Only solution, it allows you to make your buddypress only viewable to visitors that are logged in, you can just activate the plugin and finish a little setting in admin panel and it will begin the work, but if you do not setting it in back end, it works well too, it is super easy to use.
+
+== Installation ==
+
+1:Upload the BuddyPress Members only plugin to your blog
+2:Activate it
+3:Setting your register page URL or redirect page for non member of your buddypress site in BuddyPress Members only menu at admin panel
+That's all, when non member view your buddypress site they will be redirect to login/register page or if you setting your register page URL, non member will be redirected to your register page URL.
+
+== Frequently Asked Questions ==
+FAQs can be found here: http://tomas.zhu.bz/my-buddypress-plugin-buddypress-members-only.html/
+
+== Screenshots ==
+
+1. Register page URL or redirect page for non member of your buddypress site
+
+== Changelog ==
+
+= Version 1.0.0 =
+BuddyPress Members Only Published
+
+== Upgrade Notice ==
+= Version 1.0.0 =
+BuddyPress Members only Published
